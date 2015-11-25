0752e2a851cfeb7942768ade4d4b581b426d6a1d
SergeyKotyushkin <sergey.kotyushkin@firstlinesoftware.com>
2015-11-25 17:57:37 +0300

    Remove unused

--- StoreSolution.WebProject/User/MailSender/Contracts/IMailSender.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace StoreSolution.WebProject.User.MailSender.Contracts
-{
-    public interface IMailSender
-    {
-        void Send(string from, string to, string subject, string body, bool isBodyHtml);
-    }
-}
